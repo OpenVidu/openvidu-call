7430177ee8dc119370c7a7c8e609eedafe39b59f
Unknown <4a.santos@gmail.com>
2018-05-31 17:59:22 +0200

    Added fullscreen feature. Fixes #10

--- front/openvidu-call/src/app/shared/components/stream/stream.component.ts
+++ front/openvidu-call/src/app/shared/components/stream/stream.component.ts
@@ -12,6 +12,7 @@ import { OpenViduLayout } from '../../layout/openvidu-layout';
 export class StreamComponent implements OnInit {
   openviduLayout: OpenViduLayout;
   resizeTimeout;
+  fullscreenIcon = 'fullscreen';
 
   @Input() user: UserModel;
 
@@ -46,5 +47,41 @@ export class StreamComponent implements OnInit {
     const video: HTMLVideoElement = event.target;
     video.parentElement.parentElement.classList.remove('custom-class');
     this.openviduLayout.updateLayout();
+  }
+
+  toggleFullscreen() {
+    const document: any = window.document;
+    const fs = document.getElementById('container-' + this.user.stream.streamId);
+    console.log(document.getElementsByTagName('html'));
+    if (
+      !document.fullscreenElement &&
+      !document.mozFullScreenElement &&
+      !document.webkitFullscreenElement &&
+      !document.msFullscreenElement
+    ) {
+      console.log('enter FULLSCREEN!');
+      this.fullscreenIcon = 'fullscreen_exit';
+      if (fs.requestFullscreen) {
+        fs.requestFullscreen();
+      } else if (fs.msRequestFullscreen) {
+        fs.msRequestFullscreen();
+      } else if (fs.mozRequestFullScreen) {
+        fs.mozRequestFullScreen();
+      } else if (fs.webkitRequestFullscreen) {
+        fs.webkitRequestFullscreen();
+      }
+    } else {
+      console.log('exit FULLSCREEN!');
+      this.fullscreenIcon = 'fullscreen';
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      }
+    }
   }
 }
