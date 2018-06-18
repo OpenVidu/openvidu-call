ce6fd7f8e7a182518d98d0dbd212646324c29b20
nordri <nordri@gmail.com>
2018-06-18 12:06:59 +0200

    Showing results in the console

--- ci/run.sh
+++ ci/run.sh
@@ -8,6 +8,8 @@ sleep 30
 
 python /home/ubuntu/run-test.py
 
+cat /workdir/results.txt
+
 EXIT_CODE=$(grep "Process exited with error code 1" /workdir/results.txt )
 if [ -z "$EXIT_CODE" ]; then 
 	exit 0;
