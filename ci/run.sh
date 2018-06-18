c30fbe5af46d89073e0aaa4489ba7fc29b862f0d
nordri <nordri@gmail.com>
2018-06-18 11:39:55 +0200

    CI Files

---
+++ ci/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+/usr/bin/supervisord &
+sleep 2
+/usr/local/bin/startvnc.sh &
+
+sleep 30
+
+python /home/ubuntu/run-test.py
+
+EXIT_CODE=$(grep "Process exited with error code 1" /workdir/results.txt )
+if [ -z "$EXIT_CODE" ]; then 
+	exit 0;
+else 
+	exit 1;
+fi
