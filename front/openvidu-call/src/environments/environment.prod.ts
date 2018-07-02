519aa36f25d2a97df45fe54ed0e740422b8123db
Unknown <4a.santos@gmail.com>
2018-07-02 10:41:03 +0200

    Updated environments

--- front/openvidu-call/src/environments/environment.prod.ts
+++ front/openvidu-call/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  openvidu_url: '',
+  openvidu_secret: ''
 };
--- front/openvidu-call/src/environments/environment.ts
+++ front/openvidu-call/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  openvidu_url: '',
+  openvidu_secret: ''
 };
 
 /*
