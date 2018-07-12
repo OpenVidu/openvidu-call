8ec330825755fe8a0dc13f59f1392d118f4ad677
pabloFuente <pablofuenteperez@gmail.com>
2018-07-12 11:28:55 +0200

    npm run build:elements script updated (version as arg). Dependencies updated

--- front/openvidu-call/elements-build.js
+++ front/openvidu-call/elements-build.js
@@ -1,6 +1,13 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 
+if (process.argv.length != 3) {
+    console.warn("Usage: npm run build:elements -- VERSION");
+    console.warn('For example: "npm run build:elements -- 2.3.0"');
+    process.exit(-1);
+}
+var VERSION = process.argv[2];
+
 async function buildElement() {
   const files = [
     './dist/openvidu-call/runtime.js',
@@ -11,31 +18,19 @@ async function buildElement() {
 
   try {
     await fs.ensureDir('elements');
-
-    await concat(files, 'elements/openvidu-session.js');
-
-    await fs.copy('./dist/openvidu-call/styles.css', './elements/openvidu-session.css');
-
-    await fs.copy('./dist/openvidu-call/assets/images/openvidu_logo.png', './elements/assets/images/openvidu_logo.png');
-
-    await fs.copy('./dist/openvidu-call/assets/images/openvidu_logo_grey.png', './elements/assets/images/openvidu_logo_grey.png');
-
-    await fs.copy('./dist/openvidu-call/assets/images/favicon.ico', './elements/assets/images/favicon.ico');
-
+    await concat(files, 'elements/openvidu-webcomponent-' + VERSION + '.js')
+    await fs.copy('./dist/openvidu-call/styles.css', './elements/openvidu-webcomponent-' + VERSION + '.css');
   } catch (err) {
     console.error('Error executing build funtion in elements-builds.js', err);
   }
 }
 
 async function copyFiles() {
-  const destination = '../../../openvidu-tutorials/openvidu-starter/web';
+  const destination = '../../../openvidu-tutorials/openvidu-webcomponent/web';
 
   try {
-
     await fs.ensureDir('elements');
-
     await fs.copy('./elements/', destination);
-
   } catch (err) {
     console.error('Error executing copy function in elements-builds.js', err);
   }
@@ -43,11 +38,8 @@ async function copyFiles() {
 
 async function removeElement() {
   try {
-
     await fs.ensureDir('elements');
-    
     await fs.remove('./elements/');
-
   } catch (err) {
     console.error('Error executing remove function in elements-builds.js', err);
   }
