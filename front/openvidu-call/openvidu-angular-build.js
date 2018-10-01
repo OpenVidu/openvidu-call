1b3eb4a4ea6dd154cd9a2e93d78d15c29f1550d6
pabloFuente <pablofuenteperez@gmail.com>
2018-10-01 15:48:55 +0200

    openvidu-angular building script renamed

--- front/openvidu-call/openvidu-angular-build.js
+++ front/openvidu-call/openvidu-angular-build.js
@@ -52,7 +52,7 @@ async function copyFiles() {
       });
     });
   } catch (err) {
-    console.error('Error executing copy function in openvidu-angular-copy.js', err);
+    console.error('Error executing copy function in openvidu-angular-build.js', err);
   }
 }
 
@@ -67,7 +67,7 @@ async function cleanLibrary() {
     await fs.ensureDir(environmentDest);
     await fs.remove(environmentDest);
   } catch (err) {
-    console.error('Error executing clean library function in openvidu-angular-copy.js', err);
+    console.error('Error executing clean library function in openvidu-angular-build.js', err);
   }
 }
 cleanLibrary()
