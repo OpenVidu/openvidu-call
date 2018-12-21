d3109e10619fc4edfe1ed087ece27fa2b6a33de8
CSantos <4a.santos@gmail.com>
2018-12-21 11:28:10 +0100

    Fixed bug with ov-settings file

--- front/openvidu-call/src/app/shared/services/open-vidu.service.ts
+++ front/openvidu-call/src/app/shared/services/open-vidu.service.ts
@@ -6,11 +6,11 @@ import { environment } from '../../../environments/environment';
 import { OvSettings } from '../models/ov-settings';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OpenViduService {
-  private URL_OV: string;
-  private MY_SECRET: string;
+  private URL_OV = 'https://' + location.hostname + ':4443';
+  private MY_SECRET = 'MY_SECRET';
   private SETTINGS_FILE_NAME = 'ov-settings.json';
 
   private ovSettings: OvSettings = {
@@ -21,17 +21,17 @@ export class OpenViduService {
       audio: true,
       fullscreen: true,
       screenShare: true,
-      exit: true
-    }
+      exit: true,
+    },
   };
 
   constructor(private http: HttpClient) {}
 
-  getToken(mySessionId: string, openviduServerUrl: string , openviduSecret: string ): Promise<string> {
+  getToken(mySessionId: string, openviduServerUrl: string, openviduSecret: string): Promise<string> {
     const ov_url = openviduServerUrl !== undefined ? openviduServerUrl : this.URL_OV;
     const ov_secret = openviduSecret !== undefined ? openviduSecret : this.MY_SECRET;
     return this.createSession(mySessionId, ov_url, ov_secret).then((sessionId: string) => {
-        return this.createToken(sessionId, ov_url, ov_secret);
+      return this.createToken(sessionId, ov_url, ov_secret);
     });
   }
 
@@ -40,18 +40,19 @@ export class OpenViduService {
       const body = JSON.stringify({ customSessionId: sessionId });
       const options = {
         headers: new HttpHeaders({
-          'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + openviduSecret),
+          Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + openviduSecret),
           'Content-Type': 'application/json',
-        })
+        }),
       };
-      return this.http.post<any>(openviduServerUrl + '/api/sessions', body, options)
+      return this.http
+        .post<any>(openviduServerUrl + '/api/sessions', body, options)
         .pipe(
-          catchError(error => {
+          catchError((error) => {
             error.status === 409 ? resolve(sessionId) : reject(error);
             return observableThrowError(error);
-          })
+          }),
         )
-        .subscribe(response => {
+        .subscribe((response) => {
           console.log(response);
           resolve(response.id);
         });
@@ -63,18 +64,19 @@ export class OpenViduService {
       const body = JSON.stringify({ session: sessionId });
       const options = {
         headers: new HttpHeaders({
-          'Authorization': 'Basic ' + btoa('OPENVIDUAPP:' + openviduSecret),
+          Authorization: 'Basic ' + btoa('OPENVIDUAPP:' + openviduSecret),
           'Content-Type': 'application/json',
-        })
+        }),
       };
-      return this.http.post<any>(openviduServerUrl + '/api/tokens', body, options)
+      return this.http
+        .post<any>(openviduServerUrl + '/api/tokens', body, options)
         .pipe(
-          catchError(error => {
+          catchError((error) => {
             reject(error);
             return observableThrowError(error);
-          })
+          }),
         )
-        .subscribe(response => {
+        .subscribe((response) => {
           console.log(response);
           resolve(response.token);
         });
@@ -83,29 +85,29 @@ export class OpenViduService {
 
   getOvSettingsData(): Promise<OvSettings> {
     return new Promise((resolve) => {
-      this.http.get(this.SETTINGS_FILE_NAME).subscribe((data: any) => {
-        console.log('FILE', data);
-        console.log(data.openviduSettings);
-        this.ovSettings = data.openviduSettings ? data.openviduSettings : this.ovSettings;
-        this.URL_OV = data.openviduCredentials.openvidu_url;
-        this.MY_SECRET = data.openviduCredentials.openvidu_secret;
-        console.log('URL Environment', this.URL_OV);
-        resolve(data.openviduSettings);
-      },
-      (error) => {
-        console.warn('Credentials file not found ');
-        if (environment.openvidu_url) {
-          console.warn('Getting from environment ');
-          this.URL_OV = environment.openvidu_url;
-          this.MY_SECRET = environment.openvidu_secret;
-        } else {
-          console.warn('Getting default data ');
-          this.URL_OV = 'https://' + location.hostname + ':4443';
-          this.MY_SECRET = 'MY_SECRET';
-        }
-        console.log('URL Environment', this.URL_OV);
-        resolve(this.ovSettings);
-      });
+      this.http.get(this.SETTINGS_FILE_NAME).subscribe(
+        (data: any) => {
+          console.log('FILE', data);
+          console.log(data.openviduSettings);
+          this.ovSettings = data.openviduSettings ? data.openviduSettings : this.ovSettings;
+          if (data.openviduCredentials) {
+            this.URL_OV = data.openviduCredentials.openvidu_url ? data.openviduCredentials.openvidu_url : this.URL_OV;
+            this.MY_SECRET = data.openviduCredentials.openvidu_secret ? data.openviduCredentials.openvidu_secret : this.MY_SECRET;
+          }
+          console.log('URL Environment', this.URL_OV);
+          resolve(data.openviduSettings);
+        },
+        (error) => {
+          console.warn('Credentials file not found ');
+          if (environment.openvidu_url) {
+            console.warn('Getting from environment ');
+            this.URL_OV = environment.openvidu_url;
+            this.MY_SECRET = environment.openvidu_secret;
+          }
+          console.log('URL Environment', this.URL_OV);
+          resolve(this.ovSettings);
+        },
+      );
     });
   }
 }
