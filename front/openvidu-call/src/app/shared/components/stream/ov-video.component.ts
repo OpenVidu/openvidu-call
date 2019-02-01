eb8b79b43c8c44c40467dcdfe713c241d70527b1
CSantos <4a.santos@gmail.com>
2019-02-01 09:44:32 +0100

    Avoid losing information when expanding the screen share

--- front/openvidu-call/src/app/shared/components/stream/ov-video.component.ts
+++ front/openvidu-call/src/app/shared/components/stream/ov-video.component.ts
@@ -29,6 +29,12 @@ export class OpenViduVideoComponent implements AfterViewInit {
   set streamManager(streamManager: StreamManager) {
     this._streamManager = streamManager;
     if (!!this.elementRef && this._streamManager) {
+      if (this._streamManager.stream.typeOfVideo === 'SCREEN') {
+        this.elementRef.nativeElement.style.objectFit = 'contain';
+        this.elementRef.nativeElement.style.background = '#636363';
+      } else {
+        this.elementRef.nativeElement.style.objectFit = 'cover';
+      }
       this._streamManager.addVideoElement(this.elementRef.nativeElement);
     }
   }
