5394133f51799d75f2bf500af11871939a3fc082
csantosm <4a.santos@gmail.com>
2020-03-23 11:44:39 +0100

    Formated code and replaceScreenTrack event added

--- front/openvidu-call/src/app/shared/components/stream/stream.component.ts
+++ front/openvidu-call/src/app/shared/components/stream/stream.component.ts
@@ -5,117 +5,121 @@ import { NicknameMatcher } from '../../forms-matchers/nickname';
 import { UtilsService } from '../../services/utils/utils.service';
 
 @Component({
-  selector: 'stream-component',
-  styleUrls: ['./stream.component.css'],
-  templateUrl: './stream.component.html',
+	selector: 'stream-component',
+	styleUrls: ['./stream.component.css'],
+	templateUrl: './stream.component.html'
 })
 export class StreamComponent implements OnInit {
-  fullscreenIcon = 'fullscreen';
-  mutedSound: boolean;
-  toggleNickname: boolean;
-  isFullscreen: boolean;
+	fullscreenIcon = 'fullscreen';
+	mutedSound: boolean;
+	toggleNickname: boolean;
+	isFullscreen: boolean;
 
-  nicknameFormControl: FormControl;
-  matcher: NicknameMatcher;
+	nicknameFormControl: FormControl;
+	matcher: NicknameMatcher;
 
-  @Input() user: UserModel;
-  @Input() localUser: UserModel;
-  @Input() lightTheme: boolean;
-  @Input() compact: boolean;
-  @Input() chatOpened: boolean;
-  @Input() newMessagesNum: number;
-  @Input() canEditNickname: boolean;
-  @Output() nicknameClicked = new EventEmitter<any>();
-  @Output() micButtonClicked = new EventEmitter<any>();
-  @Output() camButtonClicked = new EventEmitter<any>();
-  @Output() screenShareClicked = new EventEmitter<any>();
-  @Output() stopScreenSharingClicked = new EventEmitter<any>();
-  @Output() exitButtonClicked = new EventEmitter<any>();
+	@Input() user: UserModel;
+	@Input() localUser: UserModel;
+	@Input() lightTheme: boolean;
+	@Input() compact: boolean;
+	@Input() chatOpened: boolean;
+	@Input() newMessagesNum: number;
+	@Input() canEditNickname: boolean;
+	@Output() nicknameClicked = new EventEmitter<any>();
+	@Output() micButtonClicked = new EventEmitter<any>();
+	@Output() camButtonClicked = new EventEmitter<any>();
+	@Output() screenShareClicked = new EventEmitter<any>();
+	@Output() stopScreenSharingClicked = new EventEmitter<any>();
+	@Output() exitButtonClicked = new EventEmitter<any>();
   @Output() chatButtonClicked = new EventEmitter<any>();
+  @Output() replaceScreenTrackClicked = new EventEmitter<any>();
 
-  @ViewChild('videoReference') htmlVideoElement: ElementRef;
-  @ViewChild('nicknameInput') nicknameInput: ElementRef;
+	@ViewChild('videoReference') htmlVideoElement: ElementRef;
+	@ViewChild('nicknameInput') nicknameInput: ElementRef;
 
+	constructor(private utilsSrv: UtilsService) {}
 
-  constructor(private utilsSrv: UtilsService) {}
+	@HostListener('window:resize', ['$event'])
+	sizeChange(event) {
+		const maxHeight = window.screen.height;
+		const maxWidth = window.screen.width;
+		const curHeight = window.innerHeight;
+		const curWidth = window.innerWidth;
+		if (maxWidth !== curWidth && maxHeight !== curHeight) {
+			this.isFullscreen = false;
+			this.fullscreenIcon = 'fullscreen';
+		}
+	}
 
-  @HostListener('window:resize', ['$event'])
-  sizeChange(event) {
-    const maxHeight = window.screen.height;
-    const maxWidth = window.screen.width;
-    const curHeight = window.innerHeight;
-    const curWidth = window.innerWidth;
-    if (maxWidth !== curWidth && maxHeight !== curHeight) {
-      this.isFullscreen = false;
-      this.fullscreenIcon = 'fullscreen';
-    }
-  }
+	ngOnInit() {
+		this.nicknameFormControl = new FormControl(this.user.getNickname(), [Validators.maxLength(25), Validators.required]);
+		this.matcher = new NicknameMatcher();
+	}
 
-  ngOnInit() {
-    this.nicknameFormControl = new FormControl(this.user.getNickname(), [Validators.maxLength(25), Validators.required]);
-    this.matcher = new NicknameMatcher();
-  }
+	toggleFullscreen() {
+		const state = this.utilsSrv.toggleFullscreen('container-' + this.user.getStreamManager().stream.streamId);
+		if (state === 'fullscreen') {
+			this.isFullscreen = true;
+			this.fullscreenIcon = 'fullscreen_exit';
+			if (this.chatOpened) {
+				this.chatButtonClicked.emit();
+			}
+		} else {
+			this.isFullscreen = false;
+			this.fullscreenIcon = 'fullscreen';
+		}
+	}
 
-  toggleFullscreen() {
-    const state = this.utilsSrv.toggleFullscreen('container-' + this.user.getStreamManager().stream.streamId);
-    if (state === 'fullscreen') {
-      this.isFullscreen = true;
-      this.fullscreenIcon = 'fullscreen_exit';
-      if (this.chatOpened) {
-        this.chatButtonClicked.emit();
-      }
-    } else {
-      this.isFullscreen = false;
-      this.fullscreenIcon = 'fullscreen';
-    }
-  }
+	toggleSound(): void {
+		this.mutedSound = !this.mutedSound;
+	}
 
-  toggleSound(): void {
-    this.mutedSound = !this.mutedSound;
-  }
+	toggleNicknameForm(): void {
+		if (this.canEditNickname) {
+			this.toggleNickname = !this.toggleNickname;
+			setTimeout(() => {
+				if (this.nicknameInput.nativeElement) {
+					this.nicknameInput.nativeElement.focus();
+				}
+			});
+		}
+	}
 
-  toggleNicknameForm(): void {
-    if (this.canEditNickname) {
-      this.toggleNickname = !this.toggleNickname;
-      setTimeout(() => {
-        if (this.nicknameInput.nativeElement) {
-          this.nicknameInput.nativeElement.focus();
-        }
-      });
-    }
-  }
+	eventKeyPress(event) {
+		if (event && event.keyCode === 13 && this.nicknameFormControl.valid) {
+			this.nicknameClicked.emit(this.nicknameFormControl.value);
+			this.toggleNicknameForm();
+		}
+	}
 
-  eventKeyPress(event) {
-    if (event && event.keyCode === 13 && this.nicknameFormControl.valid) {
-      this.nicknameClicked.emit(this.nicknameFormControl.value);
-      this.toggleNicknameForm();
-    }
-  }
+	toggleMic() {
+		this.micButtonClicked.emit();
+	}
 
-  toggleMic() {
-    this.micButtonClicked.emit();
-  }
+	toggleCam() {
+		this.camButtonClicked.emit();
+	}
 
-  toggleCam() {
-    this.camButtonClicked.emit();
-  }
+	screenShare() {
+		this.screenShareClicked.emit();
+	}
 
-  screenShare() {
-    this.screenShareClicked.emit();
-  }
+	replaceScreenTrack() {
+		this.replaceScreenTrackClicked.emit();
+	}
 
-  stopScreenSharing() {
-    this.stopScreenSharingClicked.emit();
-  }
+	stopScreenSharing() {
+		this.stopScreenSharingClicked.emit();
+	}
 
-  exitSession() {
-    this.exitButtonClicked.emit();
-  }
+	exitSession() {
+		this.exitButtonClicked.emit();
+	}
 
-  toggleChat() {
-    this.toggleFullscreen();
-    if (!this.chatOpened) {
-      this.chatButtonClicked.emit();
-    }
-  }
+	toggleChat() {
+		this.toggleFullscreen();
+		if (!this.chatOpened) {
+			this.chatButtonClicked.emit();
+		}
+	}
 }
