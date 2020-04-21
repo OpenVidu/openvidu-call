4022278a5e1c392c5f2dc1ea3babc62bdcabde65
csantosm <4a.santos@gmail.com>
2020-04-21 20:59:02 +0200

    Added dotenv library

--- openvidu-call-back/src/app.ts
+++ openvidu-call-back/src/app.ts
@@ -2,8 +2,12 @@
 import express from 'express';
 import { SERVER_PORT } from './config';
 import {app as callController} from './controllers/CallController';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+
+
 app.use(express.static('public'));
 app.use(express.json());
 
