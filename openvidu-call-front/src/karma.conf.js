10e6b2b5d9f1816fe4d1ac19fb82f5167cc7691e
csantosm <4a.santos@gmail.com>
2020-04-30 16:27:01 +0200

    Added chrome headless config in karma tests

--- openvidu-call-front/src/karma.conf.js
+++ openvidu-call-front/src/karma.conf.js
@@ -2,30 +2,45 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
-  });
+	config.set({
+		basePath: '',
+		frameworks: ['jasmine', '@angular-devkit/build-angular'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('@angular-devkit/build-angular/plugins/karma')
+		],
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
+		customLaunchers: {
+			ChromeHeadless: {
+				base: 'Chrome',
+				flags: [
+					'--headless',
+					'--disable-gpu',
+					'--disable-translate',
+					'--disable-extensions',
+					// Without a remote debugging port, Google Chrome exits immediately.
+					'--no-sandbox',
+					'--remote-debugging-port=9222',
+					'--js-flags="--max_old_space_size=4096"'
+				]
+			}
+		},
+		coverageIstanbulReporter: {
+			dir: require('path').join(__dirname, '../coverage'),
+			reports: ['html', 'lcovonly'],
+			fixWebpackSourcePaths: true
+		},
+		reporters: ['mocha', 'progress', 'kjhtml', 'dots'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['ChromeHeadless'],
+		singleRun: false
+	});
 };
