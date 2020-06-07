1a57521c583303ab0ec3a91e4611de5d4243645c
csantosm <4a.santos@gmail.com>
2020-06-07 16:27:38 +0200

    Updated release job

--- docker/run.sh
+++ docker/run.sh
@@ -4,34 +4,26 @@ if [[ -z "$1" ]] || [[ -z "$2" ]]; then
     if [[ -z "$1" ]]; then
         echo "RELEASE_VERSION argument is required" 1>&2
     fi
-    if [[ -z "$2" ]]; then
-        echo "BRANCH_NAME argument is required" 1>&2
-    fi
-    echo "Example of use: ./run.sh 2.14.0 master" 1>&2
+    echo "Example of use: ./run.sh 2.14.0" 1>&2
     exit 1
 fi
 
 RELEASE_VERSION=$1
-BRANCH_NAME=$2
+BRANCH_NAME=master
 CALL_BASE_HREF=/
-DEMOS_BASE_HREF=/openvidu-call/
 
 printf '\n'
 printf '\n     -------------------------------------------------------------'
 printf '\n       Installing OpenVidu Call with the following arguments:'
 printf '\n'
-printf '\n          Call container tag:  openvidu/openvidu-call:%s'  "${RELEASE_VERSION}"
-printf '\n          Demos container tag:  openvidu/openvidu-call-demos:%s'  "${RELEASE_VERSION}"
-printf '\n          Branch to build:  %s'  "${BRANCH_NAME}"
+printf '\n          Call container tag:  csantosm/openvidu-call:%s'  "${RELEASE_VERSION}"
 printf '\n     -------------------------------------------------------------'
 printf '\n'
 
-docker build -f prod.dockerfile -t openvidu/openvidu-call:${RELEASE_VERSION} --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${CALL_BASE_HREF} .
-docker build -f prod.dockerfile -t openvidu/openvidu-call:${RELEASE_VERSION}-demos --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${DEMOS_BASE_HREF} .
+docker build -f prod.dockerfile -t csantosm/openvidu-call:${RELEASE_VERSION} --build-arg BRANCH_NAME=${BRANCH_NAME} --build-arg BASE_HREF=${CALL_BASE_HREF} .
 
 printf '\n'
-printf '\n     Pushing containers to OpenVidu DockerHub'
+printf '\n     Pushing containers to CSantosM DockerHub'
 printf '\n'
 
-docker push openvidu/openvidu-call:${RELEASE_VERSION}
-docker push openvidu/openvidu-call:${RELEASE_VERSION}-demos
+docker push csantosm/openvidu-call:${RELEASE_VERSION}
