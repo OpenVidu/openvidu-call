e3b834dc768ce66e8cf8647515f9d091e941fc13
csantosm <4a.santos@gmail.com>
2020-06-07 13:27:06 +0200

    Added no sandbox flag to chromedriver

--- openvidu-call-front/e2e/protractor.conf.js
+++ openvidu-call-front/e2e/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
     {
       browserName: 'chrome',
       chromeOptions: {
-        args: ['use-fake-ui-for-media-stream', 'use-fake-device-for-media-stream'],
+        args: ['use-fake-ui-for-media-stream', 'use-fake-device-for-media-stream','--no-sandbox'],
       },
       acceptInsecureCerts : true
     },
