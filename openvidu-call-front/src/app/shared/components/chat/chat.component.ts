0d0026bba530aa7e8eeaaba5191fa334a8b00213
csantosm <4a.santos@gmail.com>
2020-07-07 12:17:28 +0200

    Fixed chat scroll bug when new messages is sent and chat was opened

--- openvidu-call-front/src/app/shared/components/chat/chat.component.ts
+++ openvidu-call-front/src/app/shared/components/chat/chat.component.ts
@@ -73,6 +73,7 @@ export class ChatComponent implements OnInit, OnDestroy {
 	private subscribeToMessages() {
 		this.chatMessageSubscription = this.chatService.messagesObs.subscribe((messages: ChatMessage[]) => {
 			this.messageList = messages;
+			this.scrollToBottom();
 		});
 	}
 
