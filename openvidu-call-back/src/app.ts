c5ff20a8ee05d9f26d4230c32bbabc06f592143b
Magnus Vikstrøm <magnus@ansur.no>
2021-02-13 14:25:22 +0100

    Add basic auth

--- openvidu-call-back/src/app.ts
+++ openvidu-call-back/src/app.ts
@@ -1,12 +1,23 @@
 
 import * as express from 'express';
-import { SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET, CALL_OPENVIDU_CERTTYPE } from './config';
+import { SERVER_PORT, OPENVIDU_URL, OPENVIDU_SECRET, CALL_OPENVIDU_CERTTYPE, USERNAME, PASSWORD } from './config';
 import {app as callController} from './controllers/CallController';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+const basicAuth = require('express-basic-auth')
 
+if (USERNAME !== undefined && PASSWORD !== undefined) {
+  app.use(basicAuth({
+    authorizer: (username, password) => {
+      const userMatches = basicAuth.safeCompare(username, USERNAME)
+      const passwordMatches = basicAuth.safeCompare(password, PASSWORD)
+      return userMatches & passwordMatches
+    },
+    challenge: true
+  }));
+}
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -19,6 +30,8 @@ app.listen(SERVER_PORT, () => {
     console.log(`OPENVIDU URL: ${OPENVIDU_URL}`);
     console.log(`OPENVIDU SECRET: ${OPENVIDU_SECRET}`);
     console.log(`CALL OPENVIDU CERTTYPE: ${CALL_OPENVIDU_CERTTYPE}`);
+    console.log(`USERNAME: ${USERNAME}`);
+    console.log(`PASSWORD: ${PASSWORD}`);
     console.log(`OpenVidu Call Server is listening on port ${SERVER_PORT}`);
     console.log(" ")
     console.log("---------------------------------------------------------");
