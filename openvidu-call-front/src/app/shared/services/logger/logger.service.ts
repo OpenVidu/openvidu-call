f8d24ddd373051f3ef12e4e647126d5aaa2bde93
cruizba <charlesrb94@outlook.com>
2021-06-24 18:15:33 +0200

    Reorder getLoggerFns calls

--- openvidu-call-front/src/app/shared/services/logger/logger.service.ts
+++ openvidu-call-front/src/app/shared/services/logger/logger.service.ts
@@ -17,29 +17,30 @@ export class LoggerService implements ILogService {
 	constructor() {
 	}
 
-	private getConsole() {
+	private getLoggerFns(prefix: string) {
 		this.log = window.console;
 		this.LOG_FNS = [this.log.log, this.log.warn, this.log.error];
+		const loggerFns = this.LOG_FNS.map((logTemplFn, i) => {
+			return logTemplFn.bind(this.log, this.MSG_PREFIXES[i][0] + prefix + this.MSG_PREFIXES[i][1]);
+		});
+		return loggerFns;
 	}
 
 	public get(prefix: string) {
 		const prodMode = environment.production;
-		this.getConsole();
-		const loggerFns = this.LOG_FNS.map((logTemplFn, i) => {
-			return logTemplFn.bind(this.log, this.MSG_PREFIXES[i][0] + prefix + this.MSG_PREFIXES[i][1]);
-		});
+		const loggerService = this;
 		return {
 			d: function(...args: any[]) {
 				if (!prodMode) {
-					loggerFns[0].apply(this.log, arguments);
+					loggerService.getLoggerFns(prefix)[0].apply(this.log, arguments);
 				}
 			},
 			w: function(...args: any[]) {
-				loggerFns[1].apply(this.log, arguments);
+				loggerService.getLoggerFns(prefix)[1].apply(this.log, arguments);
 
 			},
 			e: function(...args: any[]) {
-				loggerFns[2].apply(this.log, arguments);
+				loggerService.getLoggerFns(prefix)[2].apply(this.log, arguments);
 			}
 		};
 	}
