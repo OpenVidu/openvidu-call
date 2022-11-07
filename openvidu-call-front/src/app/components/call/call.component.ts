d04c3e7c4a092f813a5285bd59a54e6b9874126b
Carlos Santos <4a.santos@gmail.com>
2022-11-07 11:58:58 +0100

    front: Listen to onNodeCrashed event for reconnection

--- openvidu-call-front/src/app/components/call/call.component.ts
+++ openvidu-call-front/src/app/components/call/call.component.ts
@@ -20,6 +20,7 @@ export class CallComponent implements OnInit {
 	recordingError: any;
 	serverError: string = '';
 	loading: boolean = true;
+	private isDebugSession: boolean = false;
 
 	constructor(
 		private restService: RestService,
@@ -33,29 +34,24 @@ export class CallComponent implements OnInit {
 			this.sessionId = params.roomName;
 		});
 
-		let nickname: string = '';
 		// Just for debugging purposes
 		const regex = /^UNSAFE_DEBUG_USE_CUSTOM_IDS_/gm;
 		const match = regex.exec(this.sessionId);
-		if (match && match.length > 0) {
-			console.warn('DEBUGGING SESSION');
-			nickname = this.participantService.getLocalParticipant().getNickname();
-		}
+		this.isDebugSession = match && match.length > 0;
 
 		try {
-			const response = await this.restService.getTokens(this.sessionId, nickname);
-			this.recordingEnabled = response.recordingEnabled;
-			this.recordingList = response.recordings;
-			this.tokens = {
-				webcam: response.cameraToken,
-				screen: response.screenToken
-			};
+			await this.initializeTokens();
 		} catch (error) {
 			console.error(error);
 			this.serverError = error?.error?.message || error?.statusText;
 		} finally {
 			this.loading = false;
 		}
+	}
+
+	async onNodeCrashed() {
+		// Request the tokens again for reconnect to the session
+		await this.initializeTokens();
 	}
 	onLeaveButtonClicked() {
 		this.isSessionAlive = false;
@@ -84,5 +80,20 @@ export class CallComponent implements OnInit {
 		} catch (error) {
 			this.recordingError = error;
 		}
+	}
+
+	private async initializeTokens(): Promise<void> {
+		let nickname: string = '';
+		if (this.isDebugSession) {
+			console.warn('DEBUGGING SESSION');
+			nickname = this.participantService.getLocalParticipant().getNickname();
+		}
+		const response = await this.restService.getTokens(this.sessionId, nickname);
+		this.recordingEnabled = response.recordingEnabled;
+		this.recordingList = response.recordings;
+		this.tokens = {
+			webcam: response.cameraToken,
+			screen: response.screenToken
+		};
 	}
 }
