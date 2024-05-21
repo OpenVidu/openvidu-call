852d642f520e428e2fc54a0a43fdcc389b224ba2
Carlos Santos <4a.santos@gmail.com>
2024-05-21 13:28:14 +0200

    Updated to Angular 15

--- openvidu-call-front/src/app/app.module.ts
+++ openvidu-call-front/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { AppRoutingModule } from './app-routing.module';
 
 // Material
 import { CdkAccordionModule } from '@angular/cdk/accordion';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
 
 // Application Components
 import { AppComponent } from './app.component';
