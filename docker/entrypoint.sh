635486dec5e9597e834c96fa86ae4312a8636b35
cruizba <carlos.ruizbal@gmail.com>
2024-06-26 18:33:15 +0200

    Change default port to 6080

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,17 +11,17 @@ trap terminate_process TERM INT
 
 if [ -z "${LIVEKIT_URL}" ]; then
     echo "LIVEKIT_URL is required"
-    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 5000:5000 openvidu-call"
+    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 6080:6080 openvidu-call"
     exit 1
 fi
 if [ -z "${LIVEKIT_API_KEY}" ]; then
     echo "LIVEKIT_API_KEY is required"
-    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 5000:5000 openvidu-call"
+    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 6080:6080 openvidu-call"
     exit 1
 fi
 if [ -z "${LIVEKIT_API_SECRET}" ]; then
     echo "LIVEKIT_API_SECRET is required"
-    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 5000:5000 openvidu-call"
+    echo "example: docker run -e LIVEKIT_URL=https://livekit-server:7880 -e LIVEKIT_API_KEY=api_key -e LIVEKIT_API_SECRET=api_secret -p 6080:6080 openvidu-call"
     exit 1
 fi
 
--- openvidu-call-back/src/config.ts
+++ openvidu-call-back/src/config.ts
@@ -1,5 +1,5 @@
 // General server configuration
-export const SERVER_PORT = process.env.SERVER_PORT || 5000;
+export const SERVER_PORT = process.env.SERVER_PORT || 6080;
 export const CALL_PRIVATE_ACCESS = process.env.CALL_PRIVATE_ACCESS || 'false';
 export const CALL_USER = process.env.CALL_USER || 'user';
 export const CALL_SECRET = process.env.CALL_SECRET || 'user';
