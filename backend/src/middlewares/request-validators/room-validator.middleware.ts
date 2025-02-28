85cd885605a35b770e72ea7718869b1dd63a7684
Carlos Santos <4a.santos@gmail.com>
2025-02-28 12:36:39 +0100

    backend: Enhance room request validation with detailed preferences schema

--- backend/src/middlewares/request-validators/room-validator.middleware.ts
+++ backend/src/middlewares/request-validators/room-validator.middleware.ts
@@ -1,9 +1,45 @@
+import {
+	BroadcastingPreferences,
+	ChatPreferences,
+	OpenViduRoomOptions,
+	RecordingPreferences,
+	RoomPreferences,
+	VirtualBackgroundPreferences
+} from '@typings-ce';
 import { Request, Response, NextFunction } from 'express';
 import { z } from 'zod';
 
-const RoomRequestOptionsSchema = z.object({
+const RecordingPreferencesSchema: z.ZodType<RecordingPreferences> = z.object({
+	enabled: z.boolean()
+});
+
+const BroadcastingPreferencesSchema: z.ZodType<BroadcastingPreferences> = z.object({
+	enabled: z.boolean()
+});
+
+const ChatPreferencesSchema: z.ZodType<ChatPreferences> = z.object({
+	enabled: z.boolean()
+});
+
+const VirtualBackgroundPreferencesSchema: z.ZodType<VirtualBackgroundPreferences> = z.object({
+	enabled: z.boolean()
+});
+
+const RoomPreferencesSchema: z.ZodType<RoomPreferences> = z.object({
+	recordingPreferences: RecordingPreferencesSchema,
+	broadcastingPreferences: BroadcastingPreferencesSchema,
+	chatPreferences: ChatPreferencesSchema,
+	virtualBackgroundPreferences: VirtualBackgroundPreferencesSchema
+});
+
+const RoomRequestOptionsSchema: z.ZodType<OpenViduRoomOptions> = z.object({
+	expirationDate: z
+		.number()
+		.positive('End date must be a positive integer')
+		.min(Date.now(), 'End date must be in the future'),
 	roomNamePrefix: z.string().optional(),
-	expirationDate: z.number().positive('End date must be a positive integer').min(Date.now(), 'End date must be in the future')
+	preferences: RoomPreferencesSchema.optional(),
+	maxParticipants: z.number().positive('Max participants must be a positive integer').optional()
 });
 
 export const validateRoomRequest = (req: Request, res: Response, next: NextFunction) => {
