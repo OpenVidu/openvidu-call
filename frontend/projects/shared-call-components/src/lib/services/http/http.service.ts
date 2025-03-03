15b48b8cf00ed2f9feadea9bea8bf96c29d8537a
juancarmore <juancar_more2@hotmail.com>
2025-03-03 19:21:49 +0100

    frontend: Refactor HttpService to simplify requests by removing unnecessary headers and update paths for admin authentication

--- frontend/projects/shared-call-components/src/lib/services/http/http.service.ts
+++ frontend/projects/shared-call-components/src/lib/services/http/http.service.ts
@@ -13,18 +13,14 @@ export class HttpService {
 	protected pathPrefix = 'meet/api';
 	protected apiVersion = 'v1';
 
-	constructor(protected http: HttpClient) {
-		// this.baseHref = '/' + (!!window.location.pathname.split('/')[1] ? window.location.pathname.split('/')[1] + '/' : '');
-	}
+	constructor(protected http: HttpClient) {}
 
 	createRoom(options: OpenViduMeetRoomOptions): Promise<OpenViduMeetRoom> {
-		const headers = this.generateHeaders();
-		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/rooms`, options, headers);
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/rooms`, options);
 	}
 
 	deleteRoom(roomName: string): Promise<any> {
-		const headers = this.generateHeaders();
-		return this.deleteRequest(`${this.pathPrefix}/${this.apiVersion}/rooms/${roomName}`, headers);
+		return this.deleteRequest(`${this.pathPrefix}/${this.apiVersion}/rooms/${roomName}`);
 	}
 
 	listRooms(): Promise<OpenViduMeetRoom[]> {
@@ -33,13 +29,11 @@ export class HttpService {
 	}
 
 	getRoom(roomName: string): Promise<OpenViduMeetRoom> {
-		const headers = this.generateHeaders();
-		return this.getRequest(`${this.pathPrefix}/${this.apiVersion}/rooms/${roomName}`, headers);
+		return this.getRequest(`${this.pathPrefix}/${this.apiVersion}/rooms/${roomName}`);
 	}
 
 	generateParticipantToken(tokenOptions: TokenOptions): Promise<{ token: string }> {
-		const headers = this.generateHeaders();
-		return this.postRequest(`${this.pathPrefix}/participants/token`, tokenOptions, headers);
+		return this.postRequest(`${this.pathPrefix}/participants/token`, tokenOptions);
 	}
 
 	/**
@@ -48,8 +42,7 @@ export class HttpService {
 	 * @returns {Promise<GlobalPreferences>} A promise that resolves to the global preferences.
 	 */
 	getGlobalPreferences(): Promise<GlobalPreferences> {
-		const headers = this.generateHeaders();
-		return this.getRequest(`${this.pathPrefix}/preferences`, headers);
+		return this.getRequest(`${this.pathPrefix}/${this.apiVersion}/preferences`);
 	}
 
 	/**
@@ -58,8 +51,7 @@ export class HttpService {
 	 * @returns {Promise<RoomPreferences>} A promise that resolves to the room preferences.
 	 */
 	getRoomPreferences(): Promise<RoomPreferences> {
-		const headers = this.generateHeaders();
-		return this.getRequest(`${this.pathPrefix}/preferences/room`, headers);
+		return this.getRequest(`${this.pathPrefix}/${this.apiVersion}/preferences/room`);
 	}
 
 	/**
@@ -69,150 +61,76 @@ export class HttpService {
 	 * @returns A promise that resolves when the preferences have been successfully saved.
 	 */
 	saveRoomPreferences(preferences: RoomPreferences): Promise<any> {
-		const headers = this.generateHeaders();
-		return this.putRequest(`${this.pathPrefix}/preferences/room`, preferences, headers);
-	}
-
-	async getConfig() {
-		return this.getRequest(`${this.pathPrefix}/config`);
+		return this.putRequest(`${this.pathPrefix}/preferences/room`, preferences);
 	}
 
 	adminLogin(body: { username: string; password: string }): Promise<{ message: string }> {
-		return this.postRequest(`${this.pathPrefix}/admin/login`, body);
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/auth/admin/login`, body);
 	}
 
 	adminLogout(): Promise<{ message: string }> {
-		return this.postRequest(`${this.pathPrefix}/admin/logout`, {});
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/auth/admin/logout`);
+	}
+
+	adminRefresh(): Promise<{ message: string }> {
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/auth/admin/refresh`);
+	}
+
+	adminVerify(): Promise<{ message: string }> {
+		return this.getRequest(`${this.pathPrefix}/${this.apiVersion}/auth/admin/verify`);
 	}
 
 	userLogin(body: { username: string; password: string }): Promise<{ message: string }> {
-		return this.postRequest(`${this.pathPrefix}/login`, body);
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/auth/login`, body);
 	}
 
 	userLogout(): Promise<{ message: string }> {
-		return this.postRequest(`${this.pathPrefix}/logout`, {});
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/auth/logout`);
 	}
 
 	getRecordings(continuationToken?: string): Promise<{ recordings: RecordingInfo[]; continuationToken: string }> {
-		let path = `${this.pathPrefix}/admin/recordings`;
+		let path = `${this.pathPrefix}/${this.apiVersion}/recordings`;
 
 		if (continuationToken) {
 			path += `?continuationToken=${continuationToken}`;
 		}
 
-		const headers = this.generateAdminHeaders();
-
-		return this.getRequest(path, headers);
+		return this.getRequest(path);
 	}
 
 	startRecording(roomName: string): Promise<RecordingInfo> {
-		const headers = this.generateHeaders();
-
-		return this.postRequest(`${this.pathPrefix}/recordings`, { roomName }, headers);
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/recordings`, { roomName });
 	}
 
 	stopRecording(recordingId: string): Promise<RecordingInfo> {
-		const headers = this.generateHeaders();
-		return this.putRequest(`${this.pathPrefix}/recordings/${recordingId}`, {}, headers);
+		return this.putRequest(`${this.pathPrefix}/${this.apiVersion}/recordings/${recordingId}`);
 	}
 
 	deleteRecording(recordingId: string): Promise<RecordingInfo> {
-		const headers = this.generateHeaders();
-		return this.deleteRequest(`${this.pathPrefix}/recordings/${recordingId}`, headers);
-	}
-
-	deleteRecordingByAdmin(recordingId: string): Promise<RecordingInfo> {
-		const headers = this.generateAdminHeaders();
-		return this.deleteRequest(`${this.pathPrefix}/admin/recordings/${recordingId}`, headers);
+		return this.deleteRequest(`${this.pathPrefix}/${this.apiVersion}/recordings/${recordingId}`);
 	}
 
 	startBroadcasting(roomName: string, broadcastUrl: string): Promise<any> {
-		const body = { roomName, broadcastUrl };
-		const headers = this.generateHeaders();
-		return this.postRequest(`${this.pathPrefix}/broadcasts/`, body, headers);
+		return this.postRequest(`${this.pathPrefix}/${this.apiVersion}/broadcasts/`, { roomName, broadcastUrl });
 	}
 
 	stopBroadcasting(broadcastId: string): Promise<any> {
-		const headers = this.generateHeaders();
-		return this.putRequest(`${this.pathPrefix}/broadcasts/${broadcastId}`, {}, headers);
+		return this.putRequest(`${this.pathPrefix}/${this.apiVersion}/broadcasts/${broadcastId}`, {});
 	}
 
-	protected postRequest(path: string, body: any, headers?: HttpHeaders): Promise<any> {
-		try {
-			return lastValueFrom(this.http.post<any>(path, body, { headers }));
-		} catch (error: any) {
-			if (error.status === 404) {
-				throw { status: error.status, message: 'Cannot connect with backend. ' + error.url + ' not found' };
-			}
-
-			throw error;
-		}
+	protected getRequest<T>(path: string): Promise<T> {
+		return lastValueFrom(this.http.get<T>(path));
 	}
 
-	protected getRequest(path: string, headers?: HttpHeaders): any {
-		try {
-			return lastValueFrom(this.http.get(path, { headers }));
-		} catch (error: any) {
-			if (error.status === 404) {
-				throw { status: error.status, message: 'Cannot connect with backend. ' + error.url + ' not found' };
-			}
-
-			throw error;
-		}
+	protected postRequest<T>(path: string, body: any = {}): Promise<T> {
+		return lastValueFrom(this.http.post<T>(path, body));
 	}
 
-	protected deleteRequest(path: string, headers?: HttpHeaders) {
-		try {
-			return lastValueFrom(this.http.delete<any>(path, { headers }));
-		} catch (error: any) {
-			console.log(error);
-
-			if (error.status === 404) {
-				throw { status: error.status, message: 'Cannot connect with backend. ' + error.url + ' not found' };
-			}
-
-			throw error;
-		}
+	protected putRequest<T>(path: string, body: any = {}): Promise<T> {
+		return lastValueFrom(this.http.put<T>(path, body));
 	}
 
-	protected putRequest(path: string, body: any = {}, headers?: HttpHeaders) {
-		try {
-			return lastValueFrom(this.http.put<any>(path, body, { headers }));
-		} catch (error: any) {
-			if (error.status === 404) {
-				throw { status: error.status, message: 'Cannot connect with backend. ' + error.url + ' not found' };
-			}
-
-			throw error;
-		}
-	}
-
-	protected generateHeaders(): HttpHeaders {
-		const headers = new HttpHeaders({
-			'Content-Type': 'application/json'
-		});
-
-		return headers;
-	}
-
-	private generateAdminHeaders(): HttpHeaders {
-		const headers = new HttpHeaders({
-			'Content-Type': 'application/json'
-		});
-		// TODO: Fix this
-		const adminCredentials = undefined; // this.storageService.getAdminCredentials();
-
-		if (!adminCredentials) {
-			console.error('Admin credentials not found');
-			return headers;
-		}
-
-		const { username, password } = adminCredentials;
-
-		if (username && password) {
-			return headers.set('Authorization', `Basic ${btoa(`${username}:${password}`)}`);
-		}
-
-		return headers;
+	protected deleteRequest<T>(path: string): Promise<T> {
+		return lastValueFrom(this.http.delete<T>(path));
 	}
 }
--- frontend/projects/shared-call-components/src/lib/services/index.ts
+++ frontend/projects/shared-call-components/src/lib/services/index.ts
@@ -1,5 +1,6 @@
 export * from './context/context.service';
 export * from './http/http.service';
+export * from './auth/auth.service';
 export * from './global-preferences/global-preferences.service';
 export * from './room/room.service';
 export * from './notification/notification.service';
