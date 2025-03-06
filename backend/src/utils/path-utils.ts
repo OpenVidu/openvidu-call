c8cddbfaebabf12e4578769ec868b6f44b49d85f
Carlos Santos <4a.santos@gmail.com>
2025-03-06 12:58:41 +0100

    backend: Update public files path in path-utils for improved structure

--- backend/src/utils/path-utils.ts
+++ backend/src/utils/path-utils.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 // Path to the source code
 const srcPath = path.resolve(__dirname, '..');
 
-const publicFilesPath = path.join(srcPath, '../public/browser');
+const publicFilesPath = path.join(srcPath, '../public');
 const webcomponentBundlePath = path.join(srcPath, '../public/webcomponent/openvidu-meet.bundle.min.js');
 const indexHtmlPath = path.join(publicFilesPath, 'index.html');
 
